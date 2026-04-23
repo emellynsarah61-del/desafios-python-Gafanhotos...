dd2fb6047475e885b9abed8a20cc47688d08b04e
Emellyn <emellynsarah61@gmail.com>
2026-04-23 17:19:34 -0300

    Add files via upload

---
+++ desafio046.py
@@ -0,0 +1,8 @@
+import time
+
+print('Contagem regressiva para os fogos de artificio!')
+for c in range(10, -1, -1):
+
+    time.sleep(1)
+    print(c)
+print('BOOOM!!!💥')
---
+++ desafio048.py
@@ -0,0 +1,7 @@
+soma = 0
+for c in range(1, 501):
+    if c % 3 == 0 and c % 2 != 0:
+        print(c, end=' ')
+        soma += c
+
+print('\nA soma é:', soma)
